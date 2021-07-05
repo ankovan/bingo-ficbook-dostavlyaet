6e6c4791a8047cfbb81ea25c0372ca54d89d349b
ankovan <personindifferent@gmail.com>
2021-07-05 23:22:15 +0300

    added bingo checker

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ const statements = [
   "Горячие парни нарасхват", "ГГ — это автор", "Повествование меняется", "Ввод персонажа постредством столкновения с ГГ",
   "Глава начинается тем, что ГГ просыпается/Глава заканчивается, когда ГГ засыпает", "Рост/вес персонажей",
   "ГГ популярен(а)", "Физика сломалась", "Это моя первая работа", "Редкие запятые",
-  "... часть", "Не судите строго", "Описание персонажей вначале", "Диалоги = Первая буква имени + «—/:»"
+  "... часть", "Не судите строго", "Описание персонажей вначале", "Диалоги = Первая буква имени + «—/:»", "Графоманство", "Оверсайз одежда",
+  "Мысли", "Клиффхэнгеры"
 ]
 let cells;
-  
+let bingoState;
+let HEIGHT = 5;
+let WIDTH = 5;
 window.onload = function() {
+    initTable(HEIGHT, WIDTH);
     cells = document.getElementsByClassName("square");
     for(let i = 0; i < cells.length; ++i) {
       const index = Math.floor(Math.random()*statements.length)
@@ -26,11 +30,92 @@ window.onload = function() {
     
 }
 function selectCell(element) {
+  const parseId = element.id.split("_");
+  const h = parseInt(parseId[1]);
+  const w = parseInt(parseId[2]);
   const classList = [...element.classList]
   if (classList.includes("active")){
-    element.classList = ["square"]; 
+    element.classList = ["square"];
+    bingoState[h][w] = 0; 
   }
   else {
     element.classList.add("active");
+    bingoState[h][w] = 1;
   }
+  let isBingo = checkBingo();
+  console.log("bingo:",isBingo);
+  if (isBingo) {
+    wonBingo()
+  } 
+}
+function initTable(width, height) {
+  const bingoTable = document.getElementById("bingo_table");
+  bingoState = []
+  for (let h = 0; h < height; h++) {
+    bingoState[h] = [];
+    const tr = document.createElement("tr");
+    for (let w = 0; w < width; w++) {
+      bingoState[h][w] = 0;
+      const td = document.createElement("td");
+      td.classList = ["square"];
+      td.addEventListener( "click", function(e) {
+          selectCell(this);
+      });
+      td.id = `cell_${h}_${w}`
+      tr.appendChild(td);
+    }
+    bingoTable.appendChild(tr);
+  }
+}
+function checkBingo() {
+  let columnCounter = new Array(WIDTH);
+  for (let w = 0; w < WIDTH; w++) {
+    columnCounter[w] = 0;
+
+  }
+  let verticalCounter = 0;
+  let reverseVerticalCounter = 0;
+  for (let h = 0; h < HEIGHT; h++) {
+    let rowCounter = 0;
+    for (let w = 0; w < WIDTH; w++) {
+      if (bingoState[h][w] == 1){
+        rowCounter++;
+        columnCounter[w] += bingoState[h][w];
+        if (h == w) {
+          verticalCounter++;
+        }
+        if (h + w == HEIGHT - 1) {
+          reverseVerticalCounter++;
+        }
+      }
+    }
+    if (rowCounter == WIDTH) {
+      return true;
+    }
+  }
+  if (verticalCounter == HEIGHT) {
+    return true;
+  }
+  if (reverseVerticalCounter == HEIGHT) {
+    return true;
+  }
+  for (let w = 0; w < WIDTH; w++) {
+    if (columnCounter[w] == HEIGHT) {
+      return true;
+    }
+  }
+  return false
+}
+function wonBingo() {
+  const modalBg = document.querySelector('.modal-bg')
+  if (isBingo = true) {
+    modalBg.classList.add('bg-active')
+  }
+}
+function refreshThePage() {
+  location.reload();
+}
+function closeModal() {
+  const modalBg = document.querySelector('.modal-bg');
+  modalBg.classList = ["modal-bg"];
 }
